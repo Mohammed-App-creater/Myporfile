d086b4b3a86d91fba21ee8d02cc02b85862c96eb
Mohammed-App-creater <114325121+Mohammed-App-creater@users.noreply.github.com>
2023-01-03 18:20:07 +0500

    html

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$dbname = "login_db";
+$username = "root";
+$password = "";
+
+$mysqli = new mysqli(
+    hostname: $host,
+    username: $username,
+    password: $password,
+    database: $dbname,
+);
+
+if ($mysqli->connect_errno){
+    die("connection error: " . $mysqli->connect_error);
+}
+
+return $mysqli;
